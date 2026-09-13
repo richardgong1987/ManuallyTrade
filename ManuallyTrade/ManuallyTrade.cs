fd1267829f9fa46e29cd16635cf86497d4847811
H.Gong3 <h.gong3@liverpool.ac.uk>
2026-09-13 17:21:36 +0900

    updated

--- ManuallyTrade/ManuallyTrade.cs
+++ ManuallyTrade/ManuallyTrade.cs
@@ -11,8 +11,11 @@ public class ManuallyTrade : Robot {
     [Parameter("风险1%", DefaultValue = 0, MinValue = 0, Group = "空1")]
     public double Short1RiskPct { get; set; }
 
+    [Parameter("空1入场价", DefaultValue = 0, MinValue = 0, Group = "空1")]
+    public double Short1Price { get; set; }
+
     [Parameter("空1止盈价", DefaultValue = 0, MinValue = 0, Group = "空1")]
-    public double Short1TakeProfit { get; set; }
+    public double Short1TPPrice { get; set; }
 
     [Parameter("debug调试", DefaultValue = false, Group = "开发调试")]
     public bool IsDebug { get; set; }
@@ -28,7 +31,7 @@ public class ManuallyTrade : Robot {
         LaunchDebug();
 
         var riskGuard = new PdhpdlRiskGuard();
-        var planner = new PdhpdlOrderPlanner(new CAlgoSymbolModel(Symbol), riskGuard, Short1TakeProfit, Short1RiskPct);
+        var planner = new PdhpdlOrderPlanner(new CAlgoSymbolModel(Symbol), riskGuard, Short1TPPrice, Short1RiskPct);
         _orderExecutor = new PdhpdlOrderExecutor(this, SymbolName, planner, riskGuard);
 
         Print("*****MovingAverageV1 started.");
